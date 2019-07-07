4f89819f9c832d14ae92b882b479e4d3bdf593af
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-07 22:23:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_supplies_in_holidays(holiday_hash)
  #end
 all_holidays_and_supplies = []
 holiday_hash.each do |season, holiday|
-all_holidays_and_supplies << holiday_hash
+#all_holidays_and_supplies << holiday_hash
 #binding.pry
 end
 end
