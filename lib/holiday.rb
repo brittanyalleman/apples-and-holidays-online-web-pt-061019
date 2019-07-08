047790459a218db0cd6b77f0960524337a98a537
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-08 00:41:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,7 +88,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 #binding.pry
-BBQ_HOLIDAYS =[]
+bbq_holidays=[]
 holiday_hash.each do |key,value|
 
 
